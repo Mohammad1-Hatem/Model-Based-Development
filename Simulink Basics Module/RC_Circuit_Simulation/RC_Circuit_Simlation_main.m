acf62a70975c955e186ec61371af014389408d11
Mohammad Hatem <90290469+Mohammad1-Hatem@users.noreply.github.com>
2025-05-29 01:09:07 +0300

    Lab 4

---
+++ Simulink Basics Module/RC_Circuit_Simulation/RC_Circuit_Simlation_main.m
@@ -0,0 +1,33 @@
+%% Name: Mohamad Hatem Mohammad Saeed
+%% Lab_3
+%% RC Circuit Simulation
+%%
+
+clc
+clear all
+close all
+
+R = 1;
+C = 1e-6;
+
+mdl = gcs;
+
+RValues = 1:2:5;
+
+legendLabel = cell(1, numel(RValues));  % Preallocate
+
+
+for i = 1:numel(RValues) 
+
+    R = RValues(i);
+
+    res = sim(mdl);
+
+    plot(res.logsout.get("Vc").Values);
+    hold on
+    axis tight
+    legendLabel{i} = "R = " + num2str(R);
+
+end
+
+legend(legendLabel);
